e1e3c6cfcdd8fcafa719cc6a2f126d61d34a0809
dhanarajccs <dhanarajccs@gmail.com>
2023-01-23 11:44:20 +0530

    create-react-app

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# jwt-auth
+# JWT-auth
 This is an authentication with JWT (JSON Web Token) for MERN Stack Application
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h3>JWT Authentication with React</h3>
+    </div>
+  );
+}
+
+export default App;
